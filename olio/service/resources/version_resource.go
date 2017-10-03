486a9c7e85ccc8f785653297cc45b39e4fba90d6
Jason Lane <quizathaderat@yahoo.com>
2017-10-03 13:19:31 -0700

    Refactor tests to use resources package

--- olio/service/resources/version_resource.go
+++ olio/service/resources/version_resource.go
@@ -31,10 +31,10 @@ func (vr *VersionResource) AddVersionExtractor(versionExtractor VersionExtractor
 func (vr *VersionResource) Init(r *gin.Engine) {
 	log.Debug("Setting up version resource.")
 
-	r.GET("/api/version", vr.GetVersion)
+	r.GET("/api/version", vr.getVersion)
 }
 
-func (vr *VersionResource) GetVersion(c *gin.Context) {
+func (vr *VersionResource) getVersion(c *gin.Context) {
 	skeletonVersion := VERSION
 	var appVersion string
 	if vr.versionExtractor != nil {
--- olio/service/resources/version_resource_test.go
+++ olio/service/resources/version_resource_test.go
@@ -1,4 +1,4 @@
-package resources_test
+package resources
 
 import (
 	"net/http"
@@ -6,27 +6,26 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/olioapps/service-skeleton-go/olio/service/resources"
 	"github.com/stretchr/testify/assert"
 )
 
-type VersionExtractor struct {
+type Extractor struct {
 }
 
-func NewVersionExtractor() *VersionExtractor {
-	return &VersionExtractor{}
+func NewExtractor() Extractor {
+	return Extractor{}
 }
 
-func (ve VersionExtractor) GetVersion() string {
+func (e Extractor) GetVersion() string {
 	return "0.0.11"
 }
 
-func (ve VersionExtractor) GetAppName() string {
+func (e Extractor) GetAppName() string {
 	return "cx-messaging"
 }
 
 func TestVersion(t *testing.T) {
-	versionExtractor := NewVersionExtractor()
+	versionExtractor := NewExtractor()
 
 	tt := []struct {
 		name string
@@ -39,23 +38,23 @@ func TestVersion(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			if tc.name == "No extractor" {
 				router := gin.New()
-				versionResource := resources.NewVersionResource()
-				router.GET("/api/version", versionResource.GetVersion)
+				versionResource := NewVersionResource()
+				router.GET("/api/version", versionResource.getVersion)
 				req, _ := http.NewRequest("GET", "/api/version", nil)
 				res := httptest.NewRecorder()
 				router.ServeHTTP(res, req)
-				assert.Equal(t, "{\n    \"serviceFrameworkVersion\": \""+resources.VERSION+"\",\n    \"appVersion\": \"no version given\"\n}", res.Body.String())
+				assert.Equal(t, "{\n    \"serviceFrameworkVersion\": \""+VERSION+"\",\n    \"appVersion\": \"no version given\"\n}", res.Body.String())
 			}
 
 			if tc.name == "With extractor" {
 				router := gin.New()
-				versionResource := resources.NewVersionResource()
+				versionResource := NewVersionResource()
 				versionResource.AddVersionExtractor(versionExtractor)
-				router.GET("/api/version", versionResource.GetVersion)
+				router.GET("/api/version", versionResource.getVersion)
 				req, _ := http.NewRequest("GET", "/api/version", nil)
 				res := httptest.NewRecorder()
 				router.ServeHTTP(res, req)
-				assert.Equal(t, "{\n    \"serviceFrameworkVersion\": \""+resources.VERSION+"\",\n    \"appVersion\": \"cx-messaging-0.0.11\"\n}", res.Body.String())
+				assert.Equal(t, "{\n    \"serviceFrameworkVersion\": \""+VERSION+"\",\n    \"appVersion\": \"cx-messaging-0.0.11\"\n}", res.Body.String())
 			}
 		})
 	}
