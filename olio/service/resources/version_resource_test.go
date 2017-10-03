1c0ebfd3e2ec46680204b8cdf8176a5ef54f6af9
Jason Lane <quizathaderat@yahoo.com>
2017-10-03 12:10:33 -0700

    Create a new router for each test and do not hard code skeleton version

--- olio/service/resources/version_resource_test.go
+++ olio/service/resources/version_resource_test.go
@@ -10,23 +10,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type VersionExtractor struct {
+}
+
+func NewVersionExtractor() *VersionExtractor {
+	return &VersionExtractor{}
+}
+
+func (ve VersionExtractor) GetVersion() string {
+	return "0.0.11"
+}
+
+func (ve VersionExtractor) GetAppName() string {
+	return "cx-messaging"
+}
+
 func TestVersion(t *testing.T) {
-	router := gin.New()
+	versionExtractor := NewVersionExtractor()
 
 	tt := []struct {
 		name string
 	}{
-		{name: "Empty extractor"},
+		{name: "No extractor"},
+		{name: "With extractor"},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			router.GET("/api/version", resources.NewVersionResource().GetVersion)
-			req, _ := http.NewRequest("GET", "/api/version", nil)
-			res := httptest.NewRecorder()
-			router.ServeHTTP(res, req)
+			if tc.name == "No extractor" {
+				router := gin.New()
+				versionResource := resources.NewVersionResource()
+				router.GET("/api/version", versionResource.GetVersion)
+				req, _ := http.NewRequest("GET", "/api/version", nil)
+				res := httptest.NewRecorder()
+				router.ServeHTTP(res, req)
+				assert.Equal(t, "{\n    \"serviceFrameworkVersion\": \""+resources.VERSION+"\",\n    \"appVersion\": \"no version given\"\n}", res.Body.String())
+			}
 
-			assert.Equal(t, res.Body.String(), "{\n    \"serviceFrameworkVersion\": \"1.0.2\",\n    \"appVersion\": \"no version given\"\n}")
+			if tc.name == "With extractor" {
+				router := gin.New()
+				versionResource := resources.NewVersionResource()
+				versionResource.AddVersionExtractor(versionExtractor)
+				router.GET("/api/version", versionResource.GetVersion)
+				req, _ := http.NewRequest("GET", "/api/version", nil)
+				res := httptest.NewRecorder()
+				router.ServeHTTP(res, req)
+				assert.Equal(t, "{\n    \"serviceFrameworkVersion\": \""+resources.VERSION+"\",\n    \"appVersion\": \"cx-messaging-0.0.11\"\n}", res.Body.String())
+			}
 		})
 	}
 }
