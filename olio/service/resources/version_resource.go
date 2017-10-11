0deeb355af719c004b7603cfdc26125a181b5527
Jason Lane <quizathaderat@yahoo.com>
2017-10-11 16:13:57 -0700

    Increment version

--- olio/service/resources/version_resource.go
+++ olio/service/resources/version_resource.go
@@ -12,7 +12,7 @@ type VersionResource struct {
 	versionExtractor extractors.VersionExtractor
 }
 
-const VERSION = "1.0.2"
+const VERSION = "1.0.3"
 
 func NewVersionResource() *VersionResource {
 	obj := VersionResource{}
