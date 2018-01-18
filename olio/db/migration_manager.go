27798944e07b5bcdb766a6917dbf472d6deae7ae
J Irving <j@lollyshouse.ca>
2018-01-18 10:19:00 -0500

    Allow configuration of migrations table name

--- olio/db/migration_manager.go
+++ olio/db/migration_manager.go
@@ -3,16 +3,18 @@ package db
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"strconv"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/olioapps/service-skeleton-go/olio/dao"
 	"github.com/olioapps/service-skeleton-go/olio/util"
+	log "github.com/sirupsen/logrus"
 )
 
 type MigrationManager struct {
 	connectionManager *dao.ConnectionManager
 	migrations        []Migration
+	tableName         string
 }
 
 type Migration func() error
@@ -24,28 +26,25 @@ func (self *MigrationManager) getRequiredSchemaVersion() int {
 func (self *MigrationManager) getCurrentVersion() (int, error) {
 	db := self.connectionManager.GetDb()
 	var version int
-	row := db.Table("migrations").Select("version").Row()
+	row := db.Table(self.tableName).Select("version").Row()
 	row.Scan(&version)
 	return version, db.Error
 }
 
 func (self *MigrationManager) perequisites() error {
 	db := self.connectionManager.GetDb()
-	if db.Exec(`
-		CREATE TABLE IF NOT EXISTS
-		migrations (
-			version int
-		)
-	`).Error != nil {
+	sqlStr := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (version int)", self.tableName)
+	if db.Exec(sqlStr).Error != nil {
 		return db.Error
 	}
 
-	rows, error := db.Table("migrations").Select("version").Rows()
+	rows, error := db.Table(self.tableName).Select("version").Rows()
 	if error != nil {
 		return error
 	}
 	if rows == nil || rows.Err() == sql.ErrNoRows || !rows.Next() {
-		return db.Exec("INSERT INTO migrations values (?)", 0).Error
+		sqlStr = fmt.Sprintf("INSERT INTO %s values (?)", self.tableName)
+		return db.Exec(sqlStr, 0).Error
 	}
 	rows.Close()
 	return nil
@@ -53,7 +52,8 @@ func (self *MigrationManager) perequisites() error {
 
 func (self *MigrationManager) incrementVersion(targetVersion int) error {
 	db := self.connectionManager.GetDb()
-	return db.Exec("UPDATE migrations SET version = ?", targetVersion).Error
+	sqlStr := fmt.Sprintf("UPDATE %s SET version = ?", self.tableName)
+	return db.Exec(sqlStr, targetVersion).Error
 }
 
 func (self *MigrationManager) Migrate() error {
@@ -99,10 +99,18 @@ func (self *MigrationManager) Migrate() error {
 	return self.Migrate()
 }
 
-func NewMigrationManager(connectionManager *dao.ConnectionManager, migrations []Migration) *MigrationManager {
+func NewMigrationManager(connectionManager *dao.ConnectionManager, migrations []Migration, tableName ...string) *MigrationManager {
+	if len(tableName) > 1 {
+		log.Fatalf("Wrong number of args (%d), function takes 2 or 3 args", 2+len(tableName))
+	}
+
 	migrationManager := MigrationManager{}
 	migrationManager.connectionManager = connectionManager
 	migrationManager.migrations = migrations
+
+	if len(tableName) == 1 {
+		migrationManager.tableName = tableName[0]
+	}
 
 	return &migrationManager
 }
