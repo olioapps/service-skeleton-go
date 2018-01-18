e53050f0bb0234cbe9035a18d3f9f46c6e40df15
J Irving <j@lollyshouse.ca>
2018-01-18 10:32:29 -0500

    Pass through table from base API function

--- olio/api/base.go
+++ olio/api/base.go
@@ -17,8 +17,18 @@ func (api *OlioBaseCoreAPI) Init() {
 	log.Info("Initializing core api.")
 }
 
-func (api *OlioBaseCoreAPI) RunMigrations(migrations []db.Migration) {
-	migrationManager := db.NewMigrationManager(api.ConnectionManager, migrations)
+func (api *OlioBaseCoreAPI) RunMigrations(migrations []db.Migration, tableName ...string) {
+	if len(tableName) > 1 {
+		log.Fatalf("Wrong number of args (%d), function takes 1 or 2 args", 1+len(tableName))
+	}
+
+	var migrationManager *db.MigrationManager
+	if len(tableName) == 1 {
+		migrationManager = db.NewMigrationManager(api.ConnectionManager, migrations, tableName[0])
+	} else {
+		migrationManager = db.NewMigrationManager(api.ConnectionManager, migrations)
+	}
+
 	if err := migrationManager.Migrate(); err != nil {
 		log.Fatal("Failed to run migrations: ", err)
 		os.Exit(1)
